564a91848a793339684c95b1ddc69567983dc169
iridium <cool.iridiy12soul@gmail.com>
2022-08-14 20:46:39 +0300

    add telegram

--- telegram.py
+++ telegram.py
@@ -1,6 +1,9 @@
+from telebot import TeleBot
+
+
 class Telegram:
     def __init__(self, api_key: str):
-        self.api_key = api_key
+        self._bot = TeleBot(api_key)
 
     def send_message(self, chat_id: str, message: str):
-        pass
+        self._bot.send_message(chat_id=chat_id, text=message)
